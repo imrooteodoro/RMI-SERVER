17f549ed988d9498ed32f6663f74a43c07c9ccb9
imrooteodoro <adelsonteodoro67@gmail.com>
2024-06-16 21:41:50 -0300

    build: app.py

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from routes import pyroRoute
+from models import pyro5
+from controllers import pyroControl
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Está funcionando"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
