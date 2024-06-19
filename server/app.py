5b8c14de4048de75eaa44bf3e89069fcd86483e0
imrooteodoro <adelsonteodoro67@gmail.com>
2024-06-19 15:13:01 -0300

    feat: seta um  ip padrão livre

--- server/app.py
+++ server/app.py
@@ -1,10 +1,28 @@
+import socket
 import Pyro5.api
 from models.server import Servidor
 
+def get_private_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '0.0.0.0'
+    finally:
+        s.close()
+    return IP
+
+import time
+
 def main():
-    servidor = Servidor('/home/teodoro/Documents/Sistemas Distribuidos/RMI-SERVER/RMI-SERVER/server/arquivos')
-    daemon = Pyro5.api.Daemon()                
-    uri = daemon.register(servidor)   
+    # Delay to allow container network setup
+    time.sleep(5)
+
+    servidor = Servidor('/app/server/arquivos')
+    daemon = Pyro5.api.Daemon(host=get_private_ip_address())                
+    uri = daemon.register(servidor, objectId="obj_188645a453a54bbc81948816e3d3edba")   
     print("Pronto. O URI do objeto é: {0}".format(uri))
     daemon.requestLoop()
 
